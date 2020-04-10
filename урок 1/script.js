742aac703e419016efd51217f5ef18fcd9109a75
admiral-f <admiral-f@yandex.ru>
2020-04-10 18:47:54 +0300

    update js

--- урок 1/script.js
+++ урок 1/script.js
@@ -1,1 +1,4 @@
-console.log(typeof(String(4)));
+var money = prompt ("Ваш бюджет на месяц?", 'руб');
+var time = prompt("Введите дату в формате YYYY-MM-DD", '0000-00-00');
+console.log(money);
+console.log(time);

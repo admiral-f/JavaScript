4810f6bbf56a70b202f79d17d6b39447edd8878c
admiral-f <admiral-f@yandex.ru>
2020-04-10 19:12:56 +0300

    add object

--- урок 1/script.js
+++ урок 1/script.js
@@ -1,4 +1,20 @@
+'use strict';
+
 var money = prompt ("Ваш бюджет на месяц?", 'руб');
 var time = prompt("Введите дату в формате YYYY-MM-DD", '0000-00-00');
+
 console.log(money);
 console.log(time);
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    savings: false
+}
+
+let exp1 = prompt ("Введите обязательную статью расходов в этом месяце", '');
+appData.expenses.exp1 = prompt ("Во сколько обойдется?", '');
+console.log(exp1);
+console.log(appData.expenses.exp1);

5ea953cd5f0b4abe5e0a3621b1b76ea03de0f426
admiral-f <admiral-f@yandex.ru>
2020-04-13 21:39:18 +0300

    add if

--- урок 1/script.js
+++ урок 1/script.js
@@ -9,13 +9,18 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     savings: false
-}
+};
 
 for (let i = 0; i < 2; i++){
     let exp1 = prompt ("Введите обязательную статью расходов в этом месяце", ''),
     exp2 = prompt ("Во сколько обойдется?", '');
-    appData.expenses[exp1] = exp2;
-}
+    if (typeof(exp1) === 'string' && typeof(exp1) != null && typeof(exp2) != null
+        && exp1 != '' && exp2 != '' && exp1.length < 50){
+        console.log('done');
+        appData.expenses[exp1] = exp2;
+    }
+    
+};
+appData.moneyPerDay = appData.budget / 30;
 
-
-alert("бюджет на 1 день: " + appData.budget / 30 + " руб.");
+alert("бюджет на 1 день: " + appData.moneyPerDay + " руб.");

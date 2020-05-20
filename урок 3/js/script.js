4185f3878f1f2676b88c62f524653d2e77e0ff4f
admiral-f <admiral-f@yandex.ru>
2020-05-20 15:06:45 +0300

    add setClock function

--- урок 3/js/script.js
+++ урок 3/js/script.js
@@ -35,9 +35,9 @@ window.addEventListener('DOMContentLoaded', function(){
 
     //timer
 
-    let deadline = '2020-10-170';
+    let deadline = '2020-10-17';
 
-    function getTimeRemaining(endtime){
+    function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t/1000) % 60),
             minutes = Math.floor((t/1000/60) % 60),
@@ -51,5 +51,24 @@ window.addEventListener('DOMContentLoaded', function(){
         };
     }
 
-    
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
